8e6f88811ad0b17faf1536c765c8bd8f4292318f
juand2011reina-blip <juand2011reina@gmail.com>
2026-05-21 19:46:54 -0500

    Tarjetas actualizadas

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,46 @@ import SpiderMan from "./assets/spiderman.jfif"
 import "./App.css"
 
 function App() {
-  return (<> 
-    <div>App</div>
-    <Tarjeta nombre="Sonic" profesión="Héroe de la ciudad de Green Hills" imagen={Sonic}/>
-    <Tarjeta nombre="Johnny Cage" profesión="Luchador" imagen={JohnnyCage}/>
-    <Tarjeta nombre="Spider-Man" profesión="Tu amigable vecino aracnido" imagen={SpiderMan}/>
-    </>
-  )
+
+  const personajes = [{
+  nombre: "Sonic",
+  profesión: "Héroe de la ciudad de Green Hills",
+  imagen: Sonic
+  }, 
+  {nombre: "Johnny Cage",
+   profesión: "Actor de Hollywood y luchador de artes marciales",
+   imagen: JohnnyCage 
+  }, 
+  {nombre: "Spider-Man",
+   profesión: "Tu amigable vecino aracnido",
+   imagen: SpiderMan
+  }]
+
+  function saludar(nombre) {
+    alert("Hola ", nombre)
+  }
+
+  return (
+  <div className="app-container"> 
+    <h1 className="app-title">Personajes</h1>
+    <div className="grid-personajes">
+      {personajes.map((personaje, index) => {
+        return (
+          <div key={index} className="tarjeta-wrapper"> 
+            <Tarjeta 
+              nombre={personaje.nombre} 
+              profesión={personaje.profesión} 
+              imagen={personaje.imagen}   
+            />
+            <button className="btn-saludar" onClick={() => saludar(personaje.nombre)}>
+              Saludar
+            </button>
+          </div>
+        )
+      })}
+    </div>
+  </div>
+)
 }
 
 export default App
